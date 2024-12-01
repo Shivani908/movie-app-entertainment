7e76ba37787cffc48be4e0a0814a244060b31641
Shivani90 <jyotishukla05274@gmail.com>
2024-12-01 22:33:56 +0530

    nmnmn

--- src/components/partials/Sidenav.jsx
+++ src/components/partials/Sidenav.jsx
@@ -4,13 +4,13 @@ import { Link } from "react-router-dom";
 const Sidenav = () => {
     return (
         <div className="w-[20%] h-full border-r-2 border-zinc-400 p-8">
-            <h1 className="text-white font-bold text-2xl">
+            <h1 className="text-2xl text-white font-bold">
                 <i class=" text-[#6556CD] ri-movie-2-line mr-2"></i>
-                <span className="text-2xl">Movies</span>
+                {/* <span className="text-2xl">Movies</span> */}
             </h1>
 
-            <nav className="flex flex-col text-zinc-400 text-xl gap-3">
-                <h1 className="text-white font-semibold text-xl mt-10 mb-5">New Feeds</h1>
+            <nav className="flex flex-col text-zinc-400 gap-3 text-l">
+                <h1 className="text-white font-semibold text-xl my-10  mb-5">New Feeds</h1>
 
                 <Link to="/tranding" className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-la p-5">
                     <i class=" mr-2 ri-fire-fill"></i> Trandings
